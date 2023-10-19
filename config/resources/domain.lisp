efb9dc69a64413eb1e7538a5ff772920598e2bb0
Achraf Atauil <achrafatauil@hotmail.com>
2023-10-19 12:15:09 +0200

    copy extra security later from loket for GET /files

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -10,6 +10,7 @@
    of responses in the result object's meta.")
 (defparameter *max-group-sorted-properties* nil)
 (defparameter sparql:*experimental-no-application-graph-for-sudo-select-queries* t)
+(setf *fetch-all-types-in-construct-queries* t)
 
 (read-domain-file "master-subsidies-domain.lisp")
 (read-domain-file "master-submissions-domain.lisp")
@@ -17,3 +18,25 @@
 (read-domain-file "slave-besluit-domain.lisp")
 (read-domain-file "master-files-domain.lisp")
 
+;; Extra security layer to return 403 on GET /files
+;; It should be ok for mu-auth; but devs can make bugs and add files to the wrong graph (i.e. public)
+(before (:list file) (resource)
+  (let ((request-filters-on-uri
+          (some (lambda (args)
+                  (let ((components (getf args :components)))
+
+                    ;;matches /files?filter[data-container][input-from-tasks][:id:]=''
+                    (or
+                      (and (= 3 (length components))
+                           (string= (elt components 2)
+                                    ":id:"))
+
+                     ;;matches /files?filter[:uri:]=''
+                       (and (= 1 (length components))
+                           (string= (elt components 0)
+                                    ":uri:")))
+                    ))
+                (extract-filters-from-request))))
+    (if request-filters-on-uri
+        resource
+        (error 'access-denied :operation :list :resource resource :id :none))))
