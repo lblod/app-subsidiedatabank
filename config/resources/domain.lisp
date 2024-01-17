e1e88d1da96a3d8a3eff8f46a340fb3481854a2a
wolfez <wolfderechter@gmail.com>
2024-01-17 16:43:30 +0100

    add caching

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,6 +1,7 @@
 (in-package :mu-cl-resources)
 
 (defparameter *cache-count-queries* nil)
+(defparameter *cache-model-properties* t)
 (defparameter *supply-cache-headers-p* t
   "when non-nil, cache headers are supplied.  this works together with mu-cache.")
 (setf *cache-model-properties-p* t)
@@ -10,6 +11,7 @@
    of responses in the result object's meta.")
 (defparameter *max-group-sorted-properties* nil)
 (defparameter sparql:*experimental-no-application-graph-for-sudo-select-queries* t)
+(setf *fetch-all-types-in-construct-queries* t)
 
 (read-domain-file "master-subsidies-domain.lisp")
 (read-domain-file "master-submissions-domain.lisp")
